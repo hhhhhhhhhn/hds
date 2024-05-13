57c3b610234d7b1c941b48ab058b46a9c9df599a
hhhhhhhhhn <joseugarte2262005@gmail.com>
2024-05-12 22:43:45 -0400

    Add better plotting and consider column order

--- examples/simple.ts
+++ examples/simple.ts
@@ -1,8 +1,16 @@
 import {Table} from "../index"
 
 let table = Table.fromArray(Array(1000).fill(0).map((_, i) => i), "x")
-let newTable = table.addComputedColumn("y", (row) => Math.sin(row.x/20))
-console.log(newTable)
+let newTable = table.addComputedColumn("sin(x)", (row) => Math.sin(row.x/100*Math.PI)*1000)
+let newTable2 = newTable.addComputedColumn("cos(x)", (row) => Math.cos(row.x/100*Math.PI)*1000)
+let newTable3 = newTable2.addComputedColumn("id(x)", (row) => row.x)
+
+newTable3.plot("x")
 
 let csvTable = await Table.fromCSV("test.csv")
 console.log(csvTable)
+
+table = Table.fromArray(Array(10).fill(0).map((_, i) => i), "x")
+let table2 = table.addComputedColumn("sin(x)", (row) => Math.sin(row.x/2*Math.PI)*10)
+
+table2.plot("x")
--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import util from "util"
 import { Console } from "node:console"
 import { Transform } from "node:stream"
-import {plot, type Plot, type Layout, type Config} from "nodeplotlib"
+import {plot, red, green, blue, yellow, magenta, cyan} from "asciichart"
 
 const ts = new Transform({ transform(chunk, _enc, cb) { cb(null, chunk) } })
 const logger = new Console({ stdout: ts })
+
+const COLORS = Array(100).fill([blue, green, red, yellow, magenta, cyan]).flat()
+const RESET = "\x1b[0m"
 
 function tableToString(data: any): string {
 	logger.table(data)
@@ -12,10 +15,57 @@ function tableToString(data: any): string {
 }
 
 
+function widthIgnoreANSI(text: string): number {
+	return text.replaceAll("\x1b[", "").replaceAll(/\d+m/g, "").length
+}
+
+function padCenter(text: string, width: number): string {
+	let left = Math.floor((width - text.length) / 2)
+	let right = width - text.length - left
+	return " ".repeat(left) + text + " ".repeat(right)
+}
+
+function parseNumber(str: string): number {
+	let num = Number(str)
+	if (isNaN(num) && str.toLowerCase() != "nan") {
+		throw false
+	}
+	return num
+}
+
+function parseBool(str: string): boolean {
+	if (str.toLowerCase() == "true") return true
+	if (str.toLowerCase() == "false") return false
+	throw false
+}
+
+// TODO: Use dd/mm/yy
+function parseDate(str: string): Date {
+	let date = new Date(str)
+	if (date.toString() == "Invalid Date") throw false
+	return date
+}
+
+function parseColumnWithFunctions(column: string[], functions: ((name: string) => any)[]): any[] {
+	for (let fn of functions) {
+		try {return column.map(fn)}
+		catch {}
+	}
+	return column
+}
+
+function parseCSVColumn(column: string[]): any[] {
+	return parseColumnWithFunctions(column, [parseNumber, parseDate, parseBool])
+}
+
+
 export class Table<T extends Record<string, any>> {
 	rows: T[]
-	constructor(rows: T[]) {
+	headers: (keyof T)[]
+
+	constructor(rows: T[], headers: (keyof T)[] = Object.keys(rows[0])) {
 		this.rows = rows
+		this.headers = headers
 	}
 
 	static fromArray<U, S extends string>(array: U[], rowName: S): Table<{[key in S]:U}> {
@@ -24,17 +74,16 @@ export class Table<T extends Record<string, any>> {
 		return new Table(rows)
 	}
 
-	static fromColumns<T extends Record<string, any>>(columns: {[key in keyof T]: T[key][]}): Table<T> {
-		let headers = Object.keys(columns)
+	static fromColumns<T extends Record<string, any>>(columns: {[key in keyof T]: T[key][]}, headers: (keyof T)[] = Object.keys(columns)): Table<T> {
 		let rows: T[] = []
-		for (let i = 0; i < columns[0].length; i++) {
+		for (let i = 0; i < Object.values(columns)[0].length; i++) {
 			let row: any = {}
 			for (let header of headers) {
 				row[header] = columns[header][i]
 			}
 			rows.push(row)
 		}
-		return new Table(rows)
+		return new Table(rows, headers)
 	}
 
 	static async fromCSV(filename: string): Promise<Table<Record<string, any>>> {
@@ -47,20 +96,11 @@ export class Table<T extends Record<string, any>> {
 		let headers = lines[0].split(",")
 		let valueRows = lines.slice(1).map(row => row.split(",")).filter(row => row.length == headers.length)
 
-		let rows: Record<string, any>[] = []
-		for (let valueRow of valueRows) {
-			let row: any = {}
-			if (valueRow.length !== headers.length) {
-				continue
-			}
-			for (let i = 0; i < headers.length; i++) {
-				row[headers[i]] = valueRow[i]
-			}
-			rows.push(row)
-		}
-		console.log(rows)
-		// @ts-ignore
-		return new Table(rows)
+		let valueColumns = valueRows[0].map((_, i) => valueRows.map(row => row[i]))
+
+		let processedColumns = Object.fromEntries(valueColumns.map(parseCSVColumn).map((column, i) => [headers[i], column]))
+
+		return Table.fromColumns(processedColumns, headers)
 	}
 
 	addComputedColumn<U, S extends string>(columnName: S, fn: (row: T) => U): Table<T & { [key in S]: U }> {
@@ -68,7 +108,7 @@ export class Table<T extends Record<string, any>> {
 		newRows = newRows.map((row) => {
 			return {...row, [columnName]: fn(row)}
 		})
-		return new Table(newRows)
+		return new Table(newRows, [...this.headers, columnName])
 	}
 
 	addColumn<U, S extends string>(columnName: S, columnValues: U[]): Table<T & { [key in S]: U }> {
@@ -79,7 +119,7 @@ export class Table<T extends Record<string, any>> {
 		newRows = newRows.map((row, i) => {
 			return {...row, [columnName]: columnValues[i]}
 		})
-		return new Table(newRows)
+		return new Table(newRows, [...this.headers, columnName])
 	}
 
 	columns(): {[key in keyof T]: T[key][]} {
@@ -90,6 +130,14 @@ export class Table<T extends Record<string, any>> {
 		return Object.fromEntries(keys.map((key, i) => [key, values[i]]))
 	}
 
+	column<S extends keyof T>(name: S): T[S][] {
+		return this.rows.map(row => row[name])
+	}
+
+	row(index: number): T {
+		return this.rows[index]
+	}
+
 	toString(): string {
 		return tableToString(this.rows)
 	}
@@ -98,23 +146,79 @@ export class Table<T extends Record<string, any>> {
 		return tableToString(this.rows)
 	}
 
-	plot(x: keyof T, y?: (keyof T)[]) {
-		if (y === undefined) {
-			y = Object.keys(this.rows[0]).filter(k => k !== x)
+	sort(fn: (a: T, b: T) => number): Table<T> {
+		let rows = structuredClone(this.rows)
+		rows.sort(fn)
+		return new Table(rows, this.headers)
+	}
+
+	sortBy(key: keyof T): Table<T> {
+		return this.sort((a, b) => a[key] - b[key])
+	}
+
+	find(fn: (row: T) => boolean): T | undefined {
+		return this.rows.find(fn)
+	}
+
+	findIndex(fn: (row: T) => boolean): number {
+		return this.rows.findIndex(fn)
+	}
+
+	plot(x: keyof T, ys?: (keyof T)[]) {
+		const parts = process.stdout.columns - 20;
+		const height = Math.round(parts*0.25)
+
+		if (ys === undefined) {
+			ys = Object.keys(this.rows[0]).filter(k => k !== x)
 		}
-		let columns = this.columns()
-		let xValues = columns[x]
-		let yPairs = y.map(k => [k, columns[k]])
+		let sorted = this.sortBy(x)
+		let xs = sorted.column(x)
+		let [xMin, xMax] = [xs[0], xs[xs.length-1]]
+		let linearXs = Array(parts).fill(0).map((_, i) => xMin + (xMax - xMin)/parts * i)
 
-		// @ts-ignore
-		let plots: Plot[] = yPairs.map(([yName, yValues]) => {
-			return {x: xValues, y: yValues, name: yName}
-		})
+		let values: {[columnName: string]: number[]} = Object.fromEntries(ys.map(name => [name, []]))
+		for (let i = 0; i < parts; i++) {
+			let biggerRowIndex = xs.findIndex(x => x > linearXs[i])
+			if (biggerRowIndex === -1) biggerRowIndex = xs.length-1
+			let biggerRow = sorted.rows[biggerRowIndex]
 
-		// @ts-ignore
-		let layout: Layout = {title: this.constructor.name, xaxis: {title: x}, yaxis: {title: y.join(", ")}}
-		let config: Config = {}
+			let smallerRowIndex = biggerRowIndex-1
+			if (smallerRowIndex < 0) smallerRowIndex = 0
+			let smallerRow = sorted.rows[smallerRowIndex]
 
-		plot(plots, layout, config)
+			let interpolator = (biggerRow[x] - linearXs[i])/(biggerRow[x] - smallerRow[x])
+
+			for(let columnName of ys) {
+				values[columnName as string].push((1-interpolator)*biggerRow[columnName] + interpolator*smallerRow[columnName])
+			}
+		}
+
+		const X_LABEL_WIDTH = 8
+
+		let plotString = plot(
+			Object.values(values),
+			{
+				height: height,
+				colors: COLORS,
+			}
+		)
+		let plotWidth = widthIgnoreANSI(plotString.split("\n")[0])
+
+		let offset = plotString.split("").findIndex((_s, i) => plotString[i] == " " && plotString.at(i-1)?.match(/\d/)) + 2
+		let dataWidth = plotWidth - offset
+
+		let legendString = ys.map((name, i) => COLORS[i] + String(name) + RESET).join(", ")
+		legendString = `Plot of ${legendString} with respect to ${String(x)}`
+		legendString = " ".repeat(Math.max(0, (plotWidth - widthIgnoreANSI(legendString))/2)) + legendString
+
+		let xLabelAmount = Math.ceil(dataWidth / X_LABEL_WIDTH)
+		let xLabels = Array(xLabelAmount+1).fill(0).map((_, i) => (i/(dataWidth/X_LABEL_WIDTH))*(xMax-xMin) + xMin)
+		let xLabelStrings = xLabels.map(x => padCenter(x.toFixed(2), X_LABEL_WIDTH))
+
+		console.log(legendString)
+		console.log(plotString)
+		console.log(" ".repeat(offset-1) + "├" + ("─".repeat(X_LABEL_WIDTH - 1) + "┬").repeat(xLabelAmount))
+		console.log(" ".repeat(Math.floor(offset - X_LABEL_WIDTH/2)) + xLabelStrings.join(""))
+
 	}
 }
