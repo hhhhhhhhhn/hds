ea1839d8e28ff2bce7924f48ff64f1c8849ec7d9
hhhhhhhhhn <joseugarte2262005@gmail.com>
2024-05-14 19:42:11 -0400

    Optimize stocks example

--- examples/stocks.ts
+++ examples/stocks.ts
@@ -1,15 +1,16 @@
 import {Table, stats} from "../index"
 
 function createSequence(time: number, expret: number, stddev: number): number[] {
-	let price = [1]
+	let price = Array(time)
+	price[0] = 1
 	for (let i = 1; i < time; i++) {
-		price.push(price[i-1] * gaussianRandom(expret, stddev))
+		price[i] = price[i-1] * gaussianRandom(expret, stddev)
 	}
 	return price
 }
 
 const YEARS = 10
-const RUNS = 500
+const RUNS = 1000
 const PERIODS_PER_YEAR = 12
 
 const ANNUAL_RETURN = 1.1072
@@ -19,18 +20,12 @@ const RET = Math.pow(ANNUAL_RETURN, 1/PERIODS_PER_YEAR)
 const STDDEV = ANNUAL_STDDEV/Math.sqrt(PERIODS_PER_YEAR)
 const TIME = PERIODS_PER_YEAR*YEARS
 
-let time = Array(TIME).fill(0).map((_, i) => i/PERIODS_PER_YEAR)
-let table = Table.fromArray(time, "time")
+let runs = Array(RUNS).fill(0).map(() => createSequence(TIME, RET, STDDEV))
+let columns = Object.fromEntries(runs.map((r, i) => [`price ${i+1}`, r]))
+columns["year"] = Array(TIME).fill(0).map((_, i) => i/PERIODS_PER_YEAR)
+let table = Table.fromColumns(columns)
 
-for (let i = 0; i < RUNS; i++) {
-	if (i % Math.floor(RUNS/10) == 0) console.log(`${Math.round(i/RUNS*100)}%`)
-	table = table.addColumn(`price ${i+1}`, createSequence(TIME, RET, STDDEV))
-}
-
-let lastTime = structuredClone(table.row(TIME-1)) as any
-delete lastTime["time"]
-
-let returns = Object.values(lastTime) as number[]
+let returns = runs.map(r => r[r.length-1])
 
 let [min, q1, q2, q3, max] = stats.quartiles(returns)
 let [avg, stddev] = stats.avgAndStddev(returns)
@@ -41,7 +36,7 @@ let table3 = table2.addComputedColumn("med", () => q2)
 let table4 = table3.addComputedColumn("q3", () => q3)
 let table5 = table4.addComputedColumn("avg", () => avg)
 
-table5.plot("time", [...Array(10).fill(0).map((_, i) => `price ${i+1}`, ) as any, "q1", "med", "q3", "avg"])
+table5.plot("year", [...Array(10).fill(0).map((_, i) => `price ${i+1}`, ) as any, "q1", "med", "q3", "avg"])
 
 console.table({min, q1, q2, q3, max, avg, stddevPerc})
 console.log(`runs with loss: ${returns.filter(x => x < 1).length / returns.length * 100}%`)
